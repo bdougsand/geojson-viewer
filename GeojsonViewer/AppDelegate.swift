c4f602207e747a137937f8603434314ee4a6bc74
Brian Sanders <bdougsand+cfb@gmail.com>
2018-05-12 16:37:50 -0400

    Initial Commit

---
+++ GeojsonViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GeojsonViewer
+//
+//  Created by Brian Sanders on 5/14/18.
+//  Copyright © 2018 Brian Sanders. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
